1feb4a1b2ec387c2d88e8b0c3bce845e261a22e2
Amit Kumar <amitkr3301@gmail.com>
2026-03-22 18:16:48 +0530

    Creating a python file for Madhav Sales Dashboard

---
+++ Dashboard.py
@@ -0,0 +1,15 @@
+# Madhav E-Commerce Sales Dashboard
+# Importing Libraries
+import kagglehub
+from kagglehub import KaggleDatasetAdapter
+
+import numpy
+import pandas
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import io
+import base64
+import plotly.express as px
+import plotly.graph_objs as go
+from dash import Dash, html, dcc, Input, Output
